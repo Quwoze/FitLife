7045c76e5c1734d8cc18b7016c1894876c53a2b0
Quwoze <quwoze@gmail.com>
2026-08-13 19:31:35 +0300

    Исправление ошибок

--- fit_life.py
+++ fit_life.py
@@ -5,7 +5,7 @@ user_name = input("Привет! Я FitLife, как тебя зовут?\n").tit
 
 while True:
     try:
-        user_age = int(input(f"Приятно познакомиться, {user_name}, сколько тебе лет?\n"))
+        user_age = int(input(f"Сколько тебе лет?\n"))
 
         if user_age < 0:
             print("Возраст не может быть отрицательным. Попробуй ещё раз!")
@@ -20,7 +20,7 @@ while True:
         user_weight = float(input("Какой у тебя вес? (в кг)\n"))
 
         if user_weight <= 0:
-            print("Вес не может быть отрицательным или равным нулю. Попробуй ещё раз!")
+            print("Вес не может быть отрицательным или равным нулю.")
             continue
         break
 
@@ -32,27 +32,35 @@ while True:
         user_height = float(input("Какой у тебя рост? (в метрах)\n"))
 
         if user_height <= 0:
-            print("Рост не может быть отрицательным или равным нулю. Попробуй ещё раз!")
+            print("Рост не может быть отрицательным или равным нулю.")
             continue
         break
 
     except ValueError:
-        print("Похоже, ты ввёл не число. Попробуй ещё раз!")    
+        print("Похоже, ты ввёл не число. Попробуй ещё раз!")
 
 
-def bmi(weight, height):  #  calc of body mass index
+def bmi(weight, height):
     """Рассчитывает индекс массы тела (ИМТ) и округляет до 1 знака."""
     return round(weight / (height ** 2), 1)
 
-def water_ml(weight):  #  calc of the water norm (in ml)
+
+def water_ml(weight):
     """Рассчитывает рекомендуемую норму воды в мл (30 мл на кг веса)."""
     return weight * WATER_PER_KG
+
 
 b_m_i = bmi(user_weight, user_height)
 water_needed = water_ml(user_weight)
 
-print(f"Отчёт о пользователе: {user_name} ({user_age} г.)")
-print(f"Твой индекс массы тела: {b_m_i}")
-print(f"Рекомендованная норма воды в день: {water_needed} мл, {water_needed / 1000} л. \n")
+print(
+    f"Отчёт о пользователе: {user_name} ({user_age} г.)"
+    )
+print(
+    f"Твой индекс массы тела: {b_m_i}"
+    )
+print(
+    f"Рекомендованная норма воды в день: {water_needed} мл, {water_needed / 1000} л."
+    )
 
 print(f"Расчёт завершён, {user_name}! Будь здоров!")
